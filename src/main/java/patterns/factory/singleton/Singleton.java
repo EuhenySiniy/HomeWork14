2a467e68f7f1be06e709df95e18e09b224df06bf
Yevhen <ibookmack@gmail.com>
2019-04-15 09:18:46 +0300

    added singleton

---
+++ src/main/java/patterns/factory/singleton/Singleton.java
@@ -0,0 +1,15 @@
+package patterns.factory.singleton;
+
+public class Singleton {
+    public static Singleton instance = null;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
